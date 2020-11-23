49fedc3280710ceb660daf5fde865269b5ca8100
eaglesky <allenchin1990@gmail.com>
2020-11-23 00:03:20 -0800

    Init game

---
+++ js/pingpong.js
@@ -0,0 +1,47 @@
+/*(function($){
+    // data definition
+    var pingpong = {
+        paddleA: {
+            x: 50,
+            y: 100,
+            width: 20,
+            height: 70,
+        },
+
+        paddleB: {
+            x: 320,
+            y: 100,
+            width: 20,
+            height: 70,
+        },
+    };
+    // view rendering
+    function renderPaddles() {
+        $("#paddleB").css("top", pingpong.paddleB.y);
+        $("#paddleA").css("top", pingpong.paddleA.y);
+    }
+    renderPaddles();
+})(jQuery);*/
+
+$(function() {
+    // data definition
+    var pingpong = {
+        paddleA: {
+            x: 50,
+            y: 100,
+            width: 20,
+            height: 70,
+        },
+
+        paddleB: {
+            x: 320,
+            y: 100,
+            width: 20,
+            height: 70,
+        },
+    };
+
+    $("#paddleA").css("top", pingpong.paddleA.y);
+    $("#paddleB").css("top", pingpong.paddleB.y);
+
+});
