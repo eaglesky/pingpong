3d7a5ca8d9a32b67d5cc2257802a611794ba7218
eaglesky <allenchin1990@gmail.com>
2020-12-20 13:05:18 -0800

    Hit the ball with paddles

--- js/pingpong.js
+++ js/pingpong.js
@@ -50,6 +50,7 @@ $(function() {
             speed: 5,
             x: 150,
             y: 100,
+            radius: 20,
             directionX: 1,
             directionY: 1,
         },
@@ -62,6 +63,7 @@ $(function() {
     }
 
     function render() {
+        renderBall();
         renderPaddles();
         window.requestAnimationFrame(render);
     }
@@ -83,7 +85,15 @@ $(function() {
         });
     }
 
+    function gameloop() {
+        moveBall();
+    }
+
     function init() {
+        
+        // set interval to call gameloop logic in 30 FPS
+        pingpong.timer = setInterval(gameloop, 1000/30);
+
         // view rendering
         window.requestAnimationFrame(render);
         // inputs
@@ -93,4 +103,85 @@ $(function() {
     // Execute the starting point
     init();
 
+
+    function ballHitsTopBottom() {
+        var y = pingpong.ball.y + pingpong.ball.speed * pingpong.ball.directionY;
+        return y < 0 || y > pingpong.playground.height;
+    }
+        
+    function ballHitsRightWall() {
+        return pingpong.ball.x + pingpong.ball.speed * pingpong.ball.directionX > pingpong.playground.width;
+    }
+
+    function ballHitsLeftWall() {
+        return pingpong.ball.x + pingpong.ball.speed * pingpong.ball.directionX < 0;
+    }
+
+    function playerAWin() {
+        // reset the ball;
+        pingpong.ball.x = 250;
+        pingpong.ball.y = 100;
+        // update the ball location variables;
+        pingpong.ball.directionX = -1;
+    }
+
+    function playerBWin() {
+        // reset the ball;
+        pingpong.ball.x = 150;
+        pingpong.ball.y = 100;
+        pingpong.ball.directionX = 1;
+    }
+
+    function moveBall() {
+        // reference useful varaibles
+        var ball = pingpong.ball;
+
+        // check playground top/bottom boundary
+        if (ballHitsTopBottom()) {
+            // reverse direction
+            ball.directionY *= -1;
+        }
+
+        // check right
+        if (ballHitsRightWall()) {
+            playerAWin();
+        }
+        
+        // check left
+        if (ballHitsLeftWall()) {
+            playerBWin();
+        }
+
+        // check paddles here
+        // Variables for checking paddles
+        var ballX = ball.x + ball.speed * ball.directionX;
+        var ballY = ball.y + ball.speed * ball.directionY;
+        // check moving paddle here, later.
+
+        // check left paddle
+        if (ballX >= pingpong.paddleA.x && ballX < pingpong.paddleA.x + pingpong.paddleA.width) {
+            if (ballY <= pingpong.paddleA.y + pingpong.paddleA.height && ballY >= pingpong.paddleA.y) {
+                ball.directionX = 1;
+            }
+        }
+        // check right paddle
+        if (ballX + pingpong.ball.radius >= pingpong.paddleB.x && ballX < pingpong.paddleB.x + pingpong.paddleB.width) {
+            if (ballY <= pingpong.paddleB.y + pingpong.paddleB.height && ballY >= pingpong.paddleB.y) {
+                ball.directionX = -1;
+            }
+        }
+
+
+        // update the ball position data
+        ball.x += ball.speed * ball.directionX;
+        ball.y += ball.speed * ball.directionY;
+    }
+
+    function renderBall() {
+        var ball = pingpong.ball;
+        $("#ball").css({
+            "left" : ball.x + ball.speed * ball.directionX,
+            "top" : ball.y + ball.speed * ball.directionY,
+        });
+    }
 });
